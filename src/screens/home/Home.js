ddd725ddb7e85086b6b270a45fff343d46031c43
Marc Martin <marcmartinbarrios@gmail.com>
2021-10-23 13:16:30 +0200

    Edit Home.js - Transform class ExploreScreen to function

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,15 @@
-import React, { Component } from "react";
-import { Text, StyleSheet, View, FlatList, TouchableOpacity, SafeAreaView, Image, ScrollView, StatusBar } from "react-native";
+import React from "react";
+import {
+    Text,
+    View, 
+    FlatList,
+    TouchableOpacity, 
+    SafeAreaView, 
+    Image, 
+    ScrollView, 
+    StatusBar } from "react-native";
 
-import Colors from "../../theme/Colors";
-import Metrics from "../../theme/Metrics";
+import styles from "./homeStyles";
 
 const recommendations = [
     {
@@ -220,16 +227,20 @@ const fakeRecipes = [
     },
   ];
 
-class ExploreScreen extends Component {
+function ExploreScreen ({ navigation }) {
+
+  const onPressHandlerDetails = () => {
+    navigation.navigate("RecipeDetails");
+  };
   
-  renderImage = (item) => {
+  const renderImage = (item) => {
     let imageUrl = "http://via.placeholder.com/640x360";
     if (item && item.photo) {
       imageUrl = item.photo;
     }
     return <Image source={{ uri: imageUrl }} style={styles.recipeImage} />;
   };
-  renderImageBox = (item) => {
+  const renderImageBox = (item) => {
     let imageUrl = "http://via.placeholder.com/640x360";
     if (item && item.photo) {
       imageUrl = item.photo;
@@ -237,11 +248,11 @@ class ExploreScreen extends Component {
     return <Image source={{ uri: imageUrl }} style={styles.scrollerRecipe} />;
   };
 
-  renderRow = ({ item }) => {
+  const renderRow = ({ item }) => {
     return (
-    <TouchableOpacity onPress={this.onPress}>
+    <TouchableOpacity onPress={onPressHandlerDetails}>
         <View style={styles.rowContainer}>
-          <View style={styles.imageContainer}>{this.renderImage(item)}</View>
+          <View style={styles.imageContainer}>{renderImage(item)}</View>
           <View style={styles.infoContainer}>
             <Text style={styles.categoryRow}>{item.categoryName}</Text>
             <Text style={styles.titleRow}>{item.name}</Text>
@@ -262,21 +273,21 @@ class ExploreScreen extends Component {
     )
   };
 
-  keyExtractor = (item) => item._id;
+  const keyExtractor = (item) => item._id;
 
-  renderList = () => {
+  const renderList = () => {
     return (
       <FlatList
-        ListHeaderComponent={this.renderRecommended}
+        ListHeaderComponent={renderRecommended}
         showsVerticalScrollIndicator={false}
-        keyExtractor={this.keyExtractor}
+        keyExtractor={keyExtractor}
         data={fakeRecipes}
-        renderItem={this.renderRow}
+        renderItem={renderRow}
       />
     );
   };
 
-  renderRecommended = () => {
+  const renderRecommended = () => {
     return (
         <View style={styles.recommendedContainer}>
             <Text style={styles.header}>Recommended</Text>
@@ -288,8 +299,8 @@ class ExploreScreen extends Component {
             >
                 {recommendations.map(item => {
                 return (
-                    <TouchableOpacity onPress={this.onPress}>
-                        <View style={styles.recipeImageBox}>{this.renderImageBox(item)}</View>
+                    <TouchableOpacity onPress={onPressHandlerDetails}>
+                        <View style={styles.recipeImageBox}>{renderImageBox(item)}</View>
                     </TouchableOpacity>
                 )
                 })}
@@ -297,7 +308,6 @@ class ExploreScreen extends Component {
       </View>
     )
   };
-  render() {
     return (
     <SafeAreaView style={styles.mainScreen}>
       <StatusBar barStyle="dark-content" />
@@ -306,118 +316,9 @@ class ExploreScreen extends Component {
             <Text style={styles.title}>Recipes</Text>
           </View>
         </View>
-        <View style={styles.container}>{this.renderList()}</View>
+        <View style={styles.container}>{renderList()}</View>
     </SafeAreaView>
 
     );
   }
-}
 export default ExploreScreen;
-
-const styles = StyleSheet.create({
-    categoryRow: {
-        color: Colors.darkGrey,
-        fontSize: 10,
-        fontStyle: "normal",
-        fontWeight: "bold",
-        letterSpacing: 1,
-        lineHeight: 24,
-        textAlign: "left",
-    },
-    cellRow: {
-        alignItems: "center",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        marginRight: Metrics.smallSpace,
-    },
-    cellText: {
-        color: Colors.mediumGrey,
-        fontSize: 13,
-        fontStyle: "normal",
-        fontWeight: "300",
-        letterSpacing: 0.5,
-        lineHeight: 24,
-    },
-    container: {
-      alignItems: "stretch",
-      backgroundColor: Colors.mainBackground,
-      flex: 1,
-      justifyContent: "flex-start",
-    },
-    header: {
-        color: Colors.black,
-        fontWeight: "bold",
-        paddingHorizontal: Metrics.baseSpace,
-        paddingVertical: Metrics.smallSpace,
-        textAlign: "left",
-    },
-    imageContainer: {
-        height: 128,
-    },
-    infoContainer: {
-        alignItems: "stretch",
-        justifyContent: "center",
-        paddingHorizontal: Metrics.baseSpace,
-        paddingVertical: Metrics.smallSpace,
-    },
-    mainScreen: {
-      backgroundColor: Colors.mainBackground,
-      flex: 1,
-    },
-    navBar: {
-      alignItems: "center",
-      backgroundColor: Colors.whiteFull,
-      height: Metrics.navBarHeight,
-      justifyContent: "center",
-      marginTop: Metrics.statusBarHeight,
-    },
-    properties: {
-        alignItems: "center",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-    },
-    recipeImage: {
-        flex: 1,
-    },
-    recipeImageBox: {
-        height: 128,
-        width: 208,
-    },
-    recommendedContainer: {
-        alignItems: "stretch",
-        justifyContent: "center",
-        marginBottom: Metrics.baseSpace,
-    },
-    rowContainer: {
-        alignItems: "stretch",
-        backgroundColor: Colors.whiteFull,
-        borderRadius: Metrics.borderRadius,
-        justifyContent: "flex-start",
-        marginBottom: Metrics.baseSpace,
-        marginHorizontal: Metrics.baseSpace,
-        overflow: "hidden",
-    },
-    scroller: {
-        paddingRight: Metrics.baseSpace,
-    },
-    scrollerRecipe: {
-        alignItems: "center",
-        borderRadius: Metrics.borderRadius,
-        height: 128,
-        justifyContent: "center",
-        marginLeft: Metrics.baseSpace,
-        overflow: "hidden",
-    },
-    title: {
-      fontWeight: "bold",
-    },
-    titleRow: {
-        color: Colors.black,
-        fontSize: 16,
-        fontStyle: "normal",
-        fontWeight: "500",
-        letterSpacing: 0.5,
-        lineHeight: 24,
-        textAlign: "left",
-    },
-  });
