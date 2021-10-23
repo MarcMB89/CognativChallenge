ffc485895a908f09a1fb4b63e955fff800aaad8d
Marc Martin <marcmartinbarrios@gmail.com>
2021-10-23 13:17:12 +0200

    Edit Details.js - Transform class RecipeDetails to function

--- src/screens/recipes/Details.js
+++ src/screens/recipes/Details.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Text,
-  StyleSheet,
   View,
   Image,
   ScrollView,
@@ -9,27 +8,14 @@ import {
   StatusBar,
 } from "react-native";
 
-import Colors from "../../theme/Colors";
-import Metrics from "../../theme/Metrics";
+import styles from "./detailsStyle";
+import data from "../../mocks/detailMock";
 
-const data = {
-  "_id": "52944",
-  "name": "Escovitch Fish",
-  "categoryId": "1",
-  "categoryName": "Fish",
-  "duration": 11,
-  "complexity": "Hard",
-  "people": 3,
-  "recommended": 1,
-  "ingredients": "2 eggs\r\n4 tomatoes\r\nsalt\r\npepper",
-  "description":
-      "Rinse fish; rub with lemon or lime, seasoned with salt and pepper or use your favorite seasoning. I used creole seasoning. Set aside or place in the oven to keep it warm until sauce is ready.\r\n\r\nIn large skillet heat oil over medium heat, until hot, add the fish, cook each side- for about 5-7 minutes until cooked through and crispy on both sides. Remove fish and set aside. Drain oil and leave about 2-3 tablespoons of oil\r\nAdd, bay leave, garlic and ginger, stir-fry for about a minute making sure the garlic does not burn\r\nThen add onion, bell peppers, thyme, scotch bonnet, sugar, all spice-continue stirring for about 2-3 minutes. Add vinegar, mix an adjust salt and pepper according to preference. Let it simmer for about 2 more minutes. \r\n\r\nDiscard bay leave, thyme spring and serve over fish with a side of this bammy. You may make the sauce about 2 days in advance.",
-  "photo": "https://www.themealdb.com/images/media/meals/1520084413.jpg",
-};
+function RecipeDetails ({ navigation }) {
 
-export default class RecipeDetails extends Component {
-
-  render() {
+  const onPressHandlerExploreScreen = () => {
+    navigation.navigate("ExploreScreen");
+  };
 
     let imageUrl = "http://via.placeholder.com/640x360";
     if (data && data.photo) {
@@ -73,89 +59,5 @@ export default class RecipeDetails extends Component {
       </SafeAreaView>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  category: {
-    color: Colors.darkGrey,
-    fontSize: 10,
-    fontStyle: "normal",
-    fontWeight: "bold",
-    letterSpacing: 1,
-    lineHeight: 24,
-    textAlign: "center",
-  },
-  cell: {
-    alignItems: "center",
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    marginRight: Metrics.smallSpace,
-  },
-  cellText: {
-    color: Colors.mediumGrey,
-    fontSize: 13,
-    fontStyle: "normal",
-    fontWeight: "300",
-    letterSpacing: 0.5,
-    lineHeight: 24,
-  },
-  container: {
-    alignItems: "stretch",
-    backgroundColor: Colors.mainBackground,
-    flex: 1,
-    justifyContent: "flex-start",
-  },
-  darkener: {
-    backgroundColor: Colors.black30,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-    right: 0,
-    top: 0,
-    zIndex: 5,
-  },
-  description: {
-    color: Colors.darkGrey,
-    fontSize: 14,
-    lineHeight: 24,
-  },
-  header: {
-    color: Colors.black,
-    fontSize: 14,
-    lineHeight: 24,
-    textAlign: "center",
-  },
-  imageContainer: {},
-  infoBox: {
-    alignItems: "stretch",
-    alignSelf: "stretch",
-    justifyContent: "center",
-    paddingHorizontal: Metrics.baseSpace,
-    paddingVertical: Metrics.smallSpace,
-  },
-  mainScreen: {
-    backgroundColor: Colors.mainBackground,
-    flex: 1,
-  },
-  properties: {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-  },
-  recipeImage: {
-    height: 192,
-  },
-  title: {
-    fontWeight: "bold",
-  },
-  title: {
-    color: Colors.black,
-    fontSize: 16,
-    fontStyle: "normal",
-    fontWeight: "500",
-    letterSpacing: 0.5,
-    lineHeight: 24,
-    textAlign: "center",
-  },
-})
+export default RecipeDetails;
